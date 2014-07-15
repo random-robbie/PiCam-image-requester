167c2624f0cba851ea55df7768b8be61856b87ec
Robbie <txt3rob@gmail.com>
2014-07-15 12:03:08 +0100

    Update photo.php

--- photo.php
+++ photo.php
@@ -7,9 +7,16 @@
 # Donate or questions email txt3rob@gmail.com	#
 #												#
 #################################################
+GLOBAL $message;
+if ($message == "MMS")
+{
+$height = "640";
+$width = "800";
+}
+
 $filename = '' . uniqid() . '';
 $currentdir = getcwd();
-passthru ('sudo raspistill -o '.$currentdir.'/images/image_'.$filename.'.jpg -h 640 -w 800 -vf');
+passthru ('sudo raspistill -o '.$currentdir.'/images/image_'.$filename.'.jpg -h ".$height." -w ".$width." -vf');
 echo "Photo Taken File name is image_".$filename.".jpg <br>";
 $image = "".$currentdir."/images/image_".$filename.".jpg";
 
